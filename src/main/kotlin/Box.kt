adbf96965772656fb51a30ce4fc6f3a9d3500ffb
aman <aman>
2021-10-12 21:04:40 +0530

    custom getters and setters

--- src/main/kotlin/Box.kt
+++ src/main/kotlin/Box.kt
@@ -1,7 +1,19 @@
 class Box {
 //    properties
-    val length: Int = 1
-    val breadth: Int = 2
+     val length: Int = 1
+     val breadth: Int = 2
+    val area: Int
+    get() = length * breadth
+
+    var boxName: String = ""
+    set(value) {
+        if(value.length < 3){
+            println("name must be at least 3 chars")
+        }else {
+            field = value
+        }
+
+    }
 
 //    behaviour
     fun intro() = println("I am a box")
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
 fun main(){
     val box = Box();
+    box.length // getter created automatically, not accessing the property directly
+    println("area => ${box.area}")
+    box.boxName = "okkkk"
+    println("boxName => ${box.boxName}")
 }
