82b185befb33b13f5a6bae207c0c8ef34b76859b
aman <aman>
2021-10-16 21:57:06 +0530

    constructors and init block

--- src/main/kotlin/Box.kt
+++ src/main/kotlin/Box.kt
@@ -1,9 +1,23 @@
-class Box {
-//    properties
-     val length: Int = 1
-     val breadth: Int = 2
-    val area: Int
-    get() = length * breadth // visibility is same as the property
+//primary constructor
+//can have access modifier => clas Box private constructor... => but then object creation would not be allowed
+//can not write any code inside the primary constructor as it does not have a body. but init block does
+class Box (
+    val length: Int = 0,
+    val breadth: Int = 0,
+    val area: Int = 0
+) {
+
+//    as many init block as we want but one recommended
+    init {
+        println("i ran after primary ctor")
+    }
+
+//    secondary constructors were needed to overload constructor params so that different combos of params could be passed
+//    but kotlin has default args so better avoid it
+//    has to call primary ctor
+//    constructor(color: String = "Default color"): this(0,0,0) {
+//
+//    }
 
     var boxName: String = ""
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 fun main(){
-    val box = Box();
-    box.length // getter created automatically, not accessing the property directly
-    println("area => ${box.area}")
-    box.boxName = "ok"
-    println("boxName => ${box.boxName}")
+    val box = Box()
+    box.boxName = "hello"
+    println(box.boxName)
 }
