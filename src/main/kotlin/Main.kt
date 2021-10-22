57b0441855dadef56c1a0049e16cac74629be8d0
aman <aman>
2021-10-23 00:33:22 +0530

    anonymous inner class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,20 @@
-class MyApp: IDownloadListener {
-    override fun onDownloadStarted() {
-        println("download started")
-    }
-
-    override fun downloadInProgress(progress: Int) {
-        println("$progress% downloaded")
-    }
-
-    override fun fileDownloaded(file: String) {
-        println("downloaded $file")
-    }
-
-}
-
 fun main(){
     val app = MyApp()
     val downloader = Downloader()
-    downloader.downloadListener = app
+
+//    object : IDownloadListener => creating anonymous class and inheriting the interface
+    downloader.downloadListener = object : IDownloadListener {
+        override fun onDownloadStarted() {
+            println("download started")
+        }
+
+        override fun downloadInProgress(progress: Int) {
+            println("$progress% downloaded")
+        }
+
+        override fun fileDownloaded(file: String) {
+            println("downloaded $file")
+        }
+    }
     downloader.downloadFile("hello.txt")
 }
